3a01d5f3c542393ad962d18cd10aaad1a76d3577
Lyndon Maydwell <lyndon@sordina.net>
2013-08-16 16:45:36 +1000

    Line Buffer all the things

--- Uniqhash.hs
+++ Uniqhash.hs
@@ -6,6 +6,7 @@ import System.IO
 main :: IO ()
 main = hSetBuffering stdin  LineBuffering
    >>  hSetBuffering stdout LineBuffering
+   >>  hSetBuffering stderr LineBuffering
    >>  getContents
    >>= uniqmd5 . lines
    >>= mapM_ putStrLn
