a303e4e6ed4ccdfffe8272ab7f0059498808e452
Lyndon Maydwell <lyndon@sordina.net>
2016-11-02 21:02:16 +1100

    removing language extensions

--- Test/Properties/AutoMealyM.hs
+++ Test/Properties/AutoMealyM.hs
@@ -10,6 +10,7 @@ prop_arr_1 = expected == result
   result   = run $ source [1..10] ~> autoMealyM ((arrPure id &&& arr succ) >>> (arr succ *** arr pred))
   expected = [(2,1),(3,2),(4,3),(5,4),(6,5),(7,6),(8,7),(9,8),(10,9),(11,10 :: Int)]
 
+-- Should this output the last value?
 prop_scan_1 :: Bool
 prop_scan_1 = expected == result
   where
