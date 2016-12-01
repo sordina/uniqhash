c01f477bd6496e624d7013327e5634a3a5db0615
Lyndon Maydwell <lyndon@sordina.net>
2016-12-01 15:40:22 +1100

    Using machines library implementation of MealyT on github

--- Text/UniqhashMachines.hs
+++ Text/UniqhashMachines.hs
@@ -19,7 +19,7 @@ import System.IO
 import Data.Machine
 import System.IO.Machine
 import System.Directory (doesFileExist)
-import Data.Machine.MealyM
+import Data.Machine.MealyT
 import Control.Arrow
 
 import qualified Crypto.Hash         as CH
@@ -37,17 +37,17 @@ pipeline = sourceHandle byLine stdin
         ~> sinkIO putStrLn
 
 changedFiles :: ProcessT IO FilePath FilePath
-changedFiles = autoMealyM (fingerprint >>> detectChanges) ~> filterJust
+changedFiles = autoT (fingerprint >>> detectChanges) ~> filterJust
 
-fingerprint :: MealyM IO FilePath (FilePath, Maybe HASH)
+fingerprint :: MealyT IO FilePath (FilePath, Maybe HASH)
 fingerprint = arr id &&& hashPipe
 
-detectChanges :: (Ord a, Eq b, Monad m) => MealyM m (a, b) (Maybe a)
+detectChanges :: (Ord a, Eq b, Monad m) => MealyT m (a, b) (Maybe a)
 detectChanges = (arr id &&& cache) >>> arr (uncurry retrieve)
 
 -- Note: Unused in main
 emitChanges :: (Ord a, Eq b) => Process (a, b) a
-emitChanges = autoMealyM detectChanges ~> filterJust
+emitChanges = autoT detectChanges ~> filterJust
 
 filterJust :: Process (Maybe a) a
 filterJust = repeatedly $ do
@@ -55,7 +55,7 @@ filterJust = repeatedly $ do
   case m of Just v  -> yield v
             Nothing -> return ()
 
-hashPipe :: MealyM IO FilePath (Maybe HASH)
+hashPipe :: MealyT IO FilePath (Maybe HASH)
 hashPipe = arrM hash
 
 hash :: FilePath -> IO (Maybe HASH)
@@ -64,8 +64,8 @@ hash f = do
   if e then fmap (Just . CH.hash) (BS.readFile f)
        else return Nothing
 
-cache :: (Monad m, Ord k) => MealyM m (k, v) (M.Map k v)
-cache = scanMealy (flip (uncurry M.insert)) M.empty
+cache :: (Monad m, Ord k) => MealyT m (k, v) (M.Map k v)
+cache = scanMealyT (flip (uncurry M.insert)) M.empty
 
 retrieve :: (Ord k, Eq v) => (k, v) -> M.Map k v -> Maybe k
 retrieve (k,v) m | M.lookup k m == Just v = Nothing
